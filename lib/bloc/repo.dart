736c5023c00f678b0e0b0d7afca3bda062765f10
SuleymanKeles <61537773+SuleymanKeles@users.noreply.github.com>
2022-05-19 17:45:56 +0300

    add bloc packet and state managment structer
    #12

---
+++ lib/bloc/repo.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+import 'package:tiktik/data/modal/kirchen_model.dart';
+import 'package:tiktik/data/modal/product_model.dart';
+import 'package:tiktik/data/modal/user_model.dart';
+
+import 'DataApiProvider.dart';
+
+class Repository {
+  final dataApiProvider = DataApiProvider();
+
+  Future<User> fetchMovieDetail(String userId) =>
+      dataApiProvider.fetchUserDetail(userId);
+
+  Future<Kitchen> fetchMovieImages(String kitchenId) =>
+      dataApiProvider.fetchKitchenDetail(kitchenId);
+
+  Future<Product> fetchMovieList(String productId) =>
+      dataApiProvider.fetchProductDetail(productId);
+}
